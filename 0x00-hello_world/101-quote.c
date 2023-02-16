300c660c8e146a8eae1070ebe5f99893cd42783e
Siphosethu Ntsabo <sethu.ntsabo9@gmail.com>
2023-02-16 13:15:37 -0800

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - A c  program that prints a line to the standard error
  *
- *Return 0 (Success)
+ *Return 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 return (1);
 }
