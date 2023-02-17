0c7345483ad70e2388f5dd32b1467b57d04a809b
Siphosethu Ntsabo <sethu.ntsabo9@gmail.com>
2023-02-17 15:19:30 -0800

    4-print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A script that prints out the alphabet in lowerecase,
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		if (ch != 'e' && != 'q')
+			putchar(ch);
+	ch++;
+	putchar('\n`)
+		return (0);
+}
