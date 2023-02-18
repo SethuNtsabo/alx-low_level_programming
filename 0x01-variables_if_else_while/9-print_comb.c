ef812ffe45cf067e75247a3c22595f2589703a59
Siphosethu Ntsabo <sethu.ntsabo9@gmail.com>
2023-02-18 00:39:31 -0800

    9-print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
- * main - A script that prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * main - A script that prints all possible outcomes for sigke digit numbers
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 48; n < 58; n++)
-    {
-        putchar(n);
-        if (n != 57)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
