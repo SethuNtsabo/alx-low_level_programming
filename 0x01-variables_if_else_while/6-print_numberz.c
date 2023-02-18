45c3d942f07eb71f7b32eeaea3373979849e5e70
Siphosethu Ntsabo <sethu.ntsabo9@gmail.com>
2023-02-17 16:07:09 -0800

    6-print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Script to print all single digit numbers of base 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
