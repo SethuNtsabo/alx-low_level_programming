7197bfaeb5ad91872004bd2346967df3f6494a82
Siphosethu Ntsabo <sethu.ntsabo9@gmail.com>
2023-02-17 16:16:49 -0800

    7-print t

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Script that prints the lowercase alphabet in reverse
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
